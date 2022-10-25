9f354fae3a586ddc666019b4b7aadf735f14b493
yehuda pilent <pylent@g.jct.ac.il>
2022-10-25 20:39:25 +0300

    I started stage zero

---
+++ Stage0/Program9390.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace a
+{
+    partial class B
+    {
+        static void Main(string[] args)
+        {
+            Welcome9390();
+            Welcome6403();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome6403();
+        private static void Welcome9390()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
