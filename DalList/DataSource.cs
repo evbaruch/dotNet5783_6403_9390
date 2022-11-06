6de9a257a29c618171b2e5b2cfe0159b79a92837
yehuda pilent <pylent@g.jct.ac.il>
2022-11-06 10:15:37 +0200

    WTFFFFFFFFFFFFFFFFFFFFFFFFFFFFv999999999

---
+++ DalList/DataSource.cs
@@ -0,0 +1,16 @@
+﻿using DO;
+namespace Dal;
+
+internal static class DataSource
+{
+    static readonly int random;
+    internal static Enums[] arrEnums = new Enums[1];
+    internal static Order[] arrOrder = new Order[100];
+    internal static OrderItem[] arrOrderItem = new OrderItem[200];
+    internal static Product[] arrProduct = new Product[50];
+
+
+
+}
+
+
