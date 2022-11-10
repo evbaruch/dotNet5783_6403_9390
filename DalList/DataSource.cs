2f98c908384a12dc3d02096528bf0142b3d12ad8
yehuda pilent <pylent@g.jct.ac.il>
2022-11-10 11:32:07 +0200

    stage 1 DIal 0.4 s_Initialize

--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -1,21 +1,31 @@
 ﻿namespace Dal;
 using DO;
+using System.Diagnostics;
 using System.Xml.Linq;
 using static DO.Enums;
 
 internal static class DataSource
 {
-    internal static string[,] productsNames = new string[5,3] { {"angel 700","centaur 650","satyr 500" },
-                                                           {"muscles 250","brain 800","sight 300" },
-                                                           {"cancer 1000","inherited illness 900","fast healing 750" },
-                                                           {"boy 2000","girl 2000","hermaphrodite 1500" },
-                                                           {"computer brain 5000","hand weapon 400","Rick and Morty 690" } };
-    
     internal static readonly Random random = new Random();
     internal static Order[] arrOrder = new Order[100];
     internal static OrderItem[] arrOrderItem = new OrderItem[200];
     internal static Product[] arrProduct = new Product[50];
 
+    internal static string[,] productsNames = new string[5, 3] { {"angel","centaur","satyr" },
+                                                           {"muscles","brain","sight" },
+                                                           {"cancer","inherited illness","fast healing" },
+                                                           {"boy","girl","hermaphrodite" },
+                                                           {"computer brain","hand weapon","Rick and Morty" } };
+    internal static int[,] productsPrice = new int[5, 3] { { 700, 650,500},
+                                                           {250,800,300},
+                                                           {1000,900,750},
+                                                           {2000,2000,1500},
+                                                           {5000,400,690} };
+
+    static DataSource()
+    {
+        s_Initialize();
+    }
 
     internal static bool addOrder(Order a)
     {
@@ -27,7 +37,19 @@ internal static class DataSource
         }
         return false;
     }
-
+    private static Order addOrder(string CustomerName, string CustomerEmail, string CstomerAddress)
+    {
+        Order order = new Order();
+        order.ID = Config.get_ID_Order;
+        order.CustomerName = CustomerName;
+        order.CustomerEmail = CustomerEmail;
+        order.CstomerAddress = CstomerAddress;
+        order.OrderDate = DateTime.Now;
+        TimeSpan convert = TimeSpan.FromMinutes(random.Next(60, 1440));
+        order.ShipDate = DateTime.Parse(convert.ToString() + order.OrderDate.ToString());
+        order.DeliveryDate = DateTime.Parse(convert.ToString() + order.ShipDate.ToString());
+        return order;
+    }
     internal static bool addOrderItem(OrderItem a)
     {
         if(Config.I_OrderItem < arrOrderItem.Length)
@@ -38,7 +60,16 @@ internal static class DataSource
         }
         return false;
     }
-
+    private static OrderItem addOrderItem(int ProductID, int OrderID, double Price)
+    {
+        OrderItem orderItem = new OrderItem();
+        orderItem.ID = Config.get_ID_OrderItem;
+        orderItem.ProductID = ProductID;
+        orderItem.OrderID = OrderID;
+        orderItem.Price = Price;
+        orderItem.Amount = random.Next(1, 4);
+        return orderItem;
+    }
     internal static bool addProduct(Product a)
     {
         if(Config.I_Product < arrProduct.Length)
@@ -58,23 +89,45 @@ internal static class DataSource
         //כול הקישקוש הזה זה כדי לקבל קטגוריה רנדולמלית
         var v = Enum.GetValues(typeof(productsCategory));
         productsCategory eunmVal = (productsCategory)v.GetValue(random.Next(v.Length));
+        int num = random.Next(0, 3);
 
         _product.Category = eunmVal;
-        _product.Name = productsNames[(int)eunmVal, random.Next(0, 3)];
-
-
-
-
-
+        _product.Name = productsNames[(int)eunmVal,num];
+        _product.Price = productsPrice[(int)eunmVal, num];   
+        _product.InStoke = random.Next(2, 12);
 
         return _product;
     }
+
     internal static void s_Initialize()
     {
         string[] CustomerName = new string[] {"yehuda", "Batman", "Evyatar", "Rabin", "Shmuel", "Kaplan", "sapphire", "bshan", "harry","potter" };
         string[] CustomerEmail = new string[] {"yehuda@gmail.com", "Batman@gmail.com", "Evyatar@gmail.com", "Rabin@gmail.com", "Shmuel@gmail.com", "Kaplan@gmail.com", "sapphire@gmail.com", "bshan@gmail.com", "harry@gmail.com", "potter@gmail.com" };
         string[] CstomerAddress = new string[] { "Jerusalem" };
+
+        //ממלה 
+        for (int i = 0; i < 20; i++)
+        {
+            int Index = random.Next(0, 10);
+            arrOrder[i] = addOrder(CustomerName[Index], CustomerEmail[Index], CstomerAddress[Index]);
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            arrProduct[i] = addProduct();
+        }
+        for (int i = 0; i < 40; i++)
+        {
+            int Index = random.Next(0, 10);
+            int ProductID = arrProduct[Index].ID;
+            double Price = (double)arrProduct[Index].Price;
+
+            Index = random.Next(0, 20);
+            int OrderID = arrOrder[Index].ID;
+
+            arrOrderItem[i] = addOrderItem(ProductID, OrderID, Price);
+        }
     }
+
     internal static int searchOrder(int ID)
     {
         for (int i = 0; i < Config.I_Order; i++)
@@ -108,42 +161,7 @@ internal static class DataSource
 		}
         return -1;
     }
-
-    private static Order addOrder(string CustomerName, string CustomerEmail,string CstomerAddress, )
-    {
-        Order order = new Order();
-        order.ID = Config.get_ID_Order; 
-        order.CustomerName = CustomerName;
-        order.CustomerEmail = CustomerEmail;
-        order.CstomerAddress = CstomerAddress;
-        order.OrderDate = DateTime.Now;
-        TimeSpan convert = TimeSpan.FromMinutes(random.Next(60, 1440));
-        order.ShipDate = DateTime.Parse(convert.ToString() + order.OrderDate.ToString());
-        order.DeliveryDate = DateTime.Parse(convert.ToString() + order.ShipDate.ToString());
-        return order;              
-    }
-
-    private static OrderItem addOrderItem(int ProductID, int OrderID, double Price)
-    {
-        OrderItem orderItem = new OrderItem();
-        orderItem.ID = Config.get_ID_OrderItem;
-        orderItem.ProductID = ProductID;
-        orderItem.OrderID = OrderID;
-        orderItem.Price = Price;
-        orderItem.Amount = random.Next(1,4);
-        return orderItem;
-    }
-
-
-
-
-
-
-    private static void s_Initialize()
-    {
-
-    }
-
+  
     internal static class Config
     {
         internal static int I_Order = 0;
@@ -180,24 +198,4 @@ internal static class DataSource
             }
         }
     }
-
 }
-
-//private void createOrderitems()
-//{
-//    for (int i = 0; i < 10; i++)
-//    {
-//        Product? product = arrProducts[s_rand.Next(arrProduct.Count)];
-//        arrOrderItem.Add
-//        (
-//            new OrderItem
-//            {
-//                OrderID = s_rand.Next(Config.s_startOrderNumber, Config.s_startOrderNumber + arrOrder.Count),
-//                ProductID = product?.ID ?? 0,
-//                Price = product?.Price ?? 0,
-//                Amount = s_rand.Next(5),
-//            }
-//        );
-//    }
-//}
-
