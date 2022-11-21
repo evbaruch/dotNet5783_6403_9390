9a075d1541109cf13939fb511c73b70bf82b7523
yehuda pilent <pylent@g.jct.ac.il>
2022-11-21 12:58:53 +0200

    Update Stage 1 0.2

---
+++ DalFacade/DalApi/IDal.cs
@@ -0,0 +1,9 @@
+﻿using DO;
+namespace DalApi;
+
+internal class IDal
+{
+    IOrder order { get;}
+    IOrder OrderItem { get; }
+    IOrder Product { get; }
+}
--- DalFacade/DalApi/IOrderItem.cs
+++ DalFacade/DalApi/IOrderItem.cs
@@ -1,10 +1,4 @@
 ﻿using DO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace DalApi;
 
 public interface IOrderItem : ICrud<OrderItem>
