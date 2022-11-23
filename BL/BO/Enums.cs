d4206269deaa7ac45af8af19667c321fbe1b571e
yehuda pilent <pylent@g.jct.ac.il>
2022-11-23 15:00:06 +0200

    stage 2 BO 0.1

---
+++ BL/BO/Enums.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO;
+
+public class Enums
+{
+    public enum productsCategory 
+    { 
+        externalTransformation,
+        internalTransformation,
+        healing,
+        modifiedBabies,
+        cyberneticTransformation 
+    }
+
+    public enum OrderStatus
+    {
+        Status
+    }
+}
--- BL/BO/OrderTracking.cs
+++ BL/BO/OrderTracking.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BO
+namespace BO;
+
+internal class OrderTracking
 {
-    internal class OrderTracking
-    {
-    }
 }
