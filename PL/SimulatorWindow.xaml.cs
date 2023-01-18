05209871d28da43dd0eb918f1c324bde7ff1a8f8
evbaruch <evbaruch@g.jct.ac.il>
2023-01-18 15:44:55 +0200

    Stage 7 Closing delay

--- PL/SimulatorWindow.xaml.cs
+++ PL/SimulatorWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -47,20 +48,29 @@ public partial class SimulatorWindow : Window
         set { SetValue(MyButtonProperty, value); }
     }
 
+    public static readonly DependencyProperty MyEstimatedTimeProperty =
+        DependencyProperty.Register("estimatedTime", typeof(int), typeof(SimulatorWindow));
+
+    public int estimatedTime
+    {
+        get { return (int)GetValue(MyEstimatedTimeProperty); }
+        set { SetValue(MyEstimatedTimeProperty, value); }
+    }
+
     public int BackTime = 0;
 
     DispatcherTimer timer = new DispatcherTimer();
 
     BlApi.IBl? bl = BlApi.Factory.Get();
-    int estimatedTime = 0;
 
     public SimulatorWindow()
     {
         OrderCurrent = bl.Order.OrderTracking((int)bl.Order.PriorityOrder());
         Time ="00:00:00";
+        close = "Colse";
+        estimatedTime = 10;
         timer.Interval = TimeSpan.FromSeconds(1);
         timer.Tick += Timer_Tick;
-        timer.Start();
         InitializeComponent();
 
     }
@@ -81,16 +91,28 @@ public partial class SimulatorWindow : Window
         e.Cancel = false;
     }
 
-    private void Close(object sender, RoutedEventArgs e)
+    private async void Close(object sender, RoutedEventArgs e)
     {
         if (MessageBox.Show("Are you sure?", "Just making sure", MessageBoxButton.OKCancel, MessageBoxImage.Question, MessageBoxResult.Cancel) == MessageBoxResult.OK)
         {
+            timer.Stop();
             this.Closing -= Window_Closing;
-
+            await Ramaining_Time();
             this.Closing += WindowSoftClosing;
             this.Close();
         }
     }
+
+    private async Task Ramaining_Time()
+    {
+        while (estimatedTime > 0)
+        {
+            estimatedTime--;
+            close = "colsing in " + estimatedTime;
+            await Task.Delay(1000);
+        }
+    }
+
 
     private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
     {
@@ -105,6 +127,7 @@ public partial class SimulatorWindow : Window
 
     private void Start_Button(object sender, RoutedEventArgs e)
     {
+        timer.Start();
         Simulator.SubscribeToUpdateSimulation(foo);
         Simulator.StartSimulation();
     }
