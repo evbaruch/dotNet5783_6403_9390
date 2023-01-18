cc41bc78999e125497cff1252ddb3cc6bfecff50
yehuda pilent <pylent@g.jct.ac.il>
2023-01-18 13:31:37 +0200

    stage 7 Simulator

--- Simulator/Simulator.cs
+++ Simulator/Simulator.cs
@@ -1,22 +1,84 @@
-﻿using System;
+﻿using BlApi;
+using BO;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Simulator;
 
 public static class Simulator
 {
-    public static void RunningSimulation()
-    {
+    private static IBl bl = Factory.Get();
 
+    private static event EventHandler<Tuple<Order, int>>? updateSimulation;//??????????
+    private static event EventHandler? stopSimulation;//?????????????
+
+    private static volatile bool isSimulationStoped = false;
+    private static Thread? thread;
+
+    public static void SubscribeToStopSimulation(EventHandler handler)
+    {
+        stopSimulation += handler;
     }
 
-    public static void RtapingSimulation()
+    public static void SubscribeToUpdateSimulation(EventHandler<Tuple<Order, int>> handler)
     {
-
+        updateSimulation += handler;
     }
 
+    public static void UnsubscribeFromStopSimulation(EventHandler handler)
+    {
+        if (stopSimulation!.GetInvocationList().Contains(handler)) //???????????
+            stopSimulation -= handler;
+    }
 
+    public static void UnsubscribeFromUpdateSimulation(EventHandler<Tuple<Order, int>> handler)
+    {
+        if (updateSimulation!.GetInvocationList().Contains(handler)) //??????????????
+            updateSimulation -= handler;
+    }
+
+    public static void StartSimulation()
+    {
+        thread = new Thread(simulation);//{ Name = "Simulation" };
+
+        thread.Start();
+    }
+
+    public static void StopSimulation()
+    {
+        isSimulationStoped = true;
+        thread?.Interrupt();
+    }
+
+    private static void sleep(int seconds)
+    {
+        try { Thread.Sleep(seconds * 1000); } catch (ThreadInterruptedException) { }
+    }
+
+    private static void simulation()
+    {
+        while (!isSimulationStoped && bl.Order.PriorityOrder() != null)
+        {
+            var order = bl.Order.OrderDetailsRequest(bl.Order.PriorityOrder() ?? throw new NullReferenceException());
+            var timeToHandle = new Random().Next(3, 7);
+            var aproximateTime = new Random().Next(timeToHandle - 2, timeToHandle + 2);
+
+            updateSimulation?.Invoke(null, new Tuple<Order, int>(order, aproximateTime));//????????
+
+            sleep(timeToHandle);
+            if (isSimulationStoped)
+                break;
+
+            if (order.Status == BO.Enums.OrderStatus.ordered)
+                bl.Order.OrderShippingUpdate(order.ID);
+            else if (order.Status == Enums.OrderStatus.shiped)
+                bl.Order.UpdateDeliveryOrder(order.ID);
+            else
+                stopSimulation?.Invoke(null, EventArgs.Empty);
+        }
+        stopSimulation?.Invoke(null, EventArgs.Empty);
+    }
 }
